c5cd822742588b5da9d77e34b40db83465fde80e
Ryan Stauffer <88114694+drdotnet22@users.noreply.github.com>
2025-07-22 09:30:25 -0400

    Finished plate changes

--- Data/PlateServices.cs
+++ Data/PlateServices.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Routing.Matching;
+using Microsoft.EntityFrameworkCore;
 using Syncfusion.Blazor.Data;
 
 namespace ReachingOutDB.Data
@@ -25,6 +26,8 @@ namespace ReachingOutDB.Data
                     .Where(p => p.Year == year)
                     .Where(p => p.Quarter == quarter)
                     .Include(p => p.PlateAssignments)
+                    .ThenInclude(p => p.Order)
+                    .ThenInclude(o => o.Customer)
                     .OrderBy(p => p.Number)
                     .ToListAsync();
             }
@@ -41,7 +44,6 @@ namespace ReachingOutDB.Data
             try
             {
                 var sortedOrders = orders.OrderByDescending(o => o.Qty).ToList();
-                var plates = new List<Plate>();
                 var plateAssignments = new List<PlateAssignment>();
                 int plateNumber = 0;
                 int year = orders.FirstOrDefault().Year;
@@ -52,9 +54,8 @@ namespace ReachingOutDB.Data
                 int totalBlanksNeeded = lowQtyOrders.Sum(o => o.Qty) + 1500;
                 int blankCount = 0;
 
-                int currentJobIndex = 0;
 
-                while (sortedOrders[currentJobIndex + 1].Qty > 950)
+                while (sortedOrders[0].Qty > 950)
                 {
                     var plate = new Plate()
                     {
@@ -64,48 +65,146 @@ namespace ReachingOutDB.Data
                     };
 
                     int platePosition = 0;
-
-                    // Add blanks if needed
-                    if (blankCount < totalBlanksNeeded)
+                    int orderIndex = 0;
+                    sortedOrders.OrderByDescending(o => o.Qty);
+                    // If largest quantity is more than 50% more or 1,000 sheets more than 2nd order
+                    if (sortedOrders[orderIndex].Qty > (sortedOrders[orderIndex + 1].Qty * 1.5) || sortedOrders[orderIndex].Qty > (sortedOrders[orderIndex + 1].Qty + 1000))
                     {
-                        blankCount += sortedOrders[currentJobIndex + 1].Qty;
-                        plate.Quantity = sortedOrders[currentJobIndex + 1].Qty;
+                        plate.Quantity = sortedOrders[orderIndex].Qty / 2;
+                        var targetQty = sortedOrders[orderIndex].Qty / 2;
+                        var plateAssignment1 = new PlateAssignment()
+                        {
+                            Plate = plate,
+                            Position = 1,
+                            OrderId = sortedOrders[orderIndex].OrderId,
+                            IsBlank = false
+                        };
+                        plateAssignments.Add(plateAssignment1);
 
-                        while (platePosition < 3)
+                        var plateAssignment2 = new PlateAssignment()
+                        {
+                            Plate = plate,
+                            Position = 2,
+                            OrderId = sortedOrders[orderIndex].OrderId,
+                            IsBlank = false
+                        };
+                        sortedOrders.Remove(sortedOrders[orderIndex]);
+                        plateAssignments.Add(plateAssignment2);
+
+                        var candidates = sortedOrders.Where(o => o.Qty <= targetQty).OrderByDescending(o => o.Qty).Take(2).ToList();
+                        foreach (var order in candidates)
                         {
                             var plateAssignment = new PlateAssignment()
                             {
                                 Plate = plate,
-                                Position = platePosition++,
-                                OrderId = sortedOrders[currentJobIndex++].OrderId,
+                                Position = 3 + candidates.IndexOf(order),
+                                OrderId = order.OrderId,
                                 IsBlank = false
                             };
+                            sortedOrders.Remove(order);
                             plateAssignments.Add(plateAssignment);
-                            sortedOrders.Remove(sortedOrders[currentJobIndex]);
+                        }
+                    }
+
+                    // Add blanks if needed and order is 10% more than 4th order
+                    else if (blankCount < totalBlanksNeeded && sortedOrders[orderIndex].Qty > (sortedOrders[orderIndex + 3].Qty * 1.1))
+                    {
+                        //If 3rd job is more than 20% or 400 more than first job, do two blank sheets
+                        if (sortedOrders[orderIndex].Qty > (sortedOrders[orderIndex + 2].Qty * 1.2) || sortedOrders[orderIndex].Qty > (sortedOrders[orderIndex + 2].Qty + 400))
+                        {
+                            plate.Quantity = sortedOrders[orderIndex].Qty;
+                            blankCount += sortedOrders[orderIndex].Qty * 2;
+                            var candidates = sortedOrders.Take(2).ToList();
+                            foreach (var order in candidates)
+                            {
+                                var plateAssignment = new PlateAssignment()
+                                {
+                                    Plate = plate,
+                                    Position = 1 + candidates.IndexOf(order),
+                                    OrderId = order.OrderId,
+                                    IsBlank = false
+                                };
+                                sortedOrders.Remove(order);
+                                plateAssignments.Add(plateAssignment);
+                            }
+                            for (var i = 3; i <= 4; i++)
+                            {
+                                var plateAssignment = new PlateAssignment()
+                                {
+                                    Plate = plate,
+                                    Position = i,
+                                    IsBlank = true
+                                };
+                                plateAssignments.Add(plateAssignment);
+                            }
+                        }
+                        else // one blank
+                        {
+                            plate.Quantity = sortedOrders[orderIndex].Qty;
+                            blankCount += sortedOrders[orderIndex].Qty;
+                            var candidates = sortedOrders.Take(3).ToList();
+                            foreach (var order in candidates)
+                            {
+                                var plateAssignment = new PlateAssignment()
+                                {
+                                    Plate = plate,
+                                    Position = 1 + candidates.IndexOf(order),
+                                    OrderId = order.OrderId,
+                                    IsBlank = false
+                                };
+                                sortedOrders.Remove(order);
+                                plateAssignments.Add(plateAssignment);
+                            }
+                            var blankPlateAssignment = new PlateAssignment()
+                            {
+                                Plate = plate,
+                                Position = 4,
+                                IsBlank = true
+                            };
+                            plateAssignments.Add(blankPlateAssignment);
+                        }
+                    }
+                    //If blanks are still needed AND order is 4% more than 4th order AND quantity of order is less than 3,000
+                    else if (blankCount < totalBlanksNeeded && sortedOrders[orderIndex].Qty > (sortedOrders[orderIndex + 3].Qty * 1.04) && sortedOrders[orderIndex].Qty < 3000)
+                    {
+                        plate.Quantity = sortedOrders[orderIndex].Qty;
+                        blankCount += sortedOrders[orderIndex].Qty;
+                        var candidates = sortedOrders.Take(3).ToList();
+                        foreach (var order in candidates)
+                        {
+                            var plateAssignment = new PlateAssignment()
+                            {
+                                Plate = plate,
+                                Position = 1 + candidates.IndexOf(order),
+                                OrderId = order.OrderId,
+                                IsBlank = false
+                            };
+                            sortedOrders.Remove(order);
+                            plateAssignments.Add(plateAssignment);
                         }
                         var blankPlateAssignment = new PlateAssignment()
                         {
                             Plate = plate,
-                            Position = platePosition++,
+                            Position = 4,
                             IsBlank = true
                         };
                         plateAssignments.Add(blankPlateAssignment);
                     }
                     else //just add 4 jobs to plate
                     {
-                        plate.Quantity = sortedOrders[currentJobIndex + 1].Qty;
-
-                        while (platePosition < 4)
+                        plate.Quantity = sortedOrders[orderIndex].Qty;
+                        var candidates = sortedOrders.Take(4).ToList();
+                        foreach (var order in candidates)
                         {
                             var plateAssignment = new PlateAssignment()
                             {
                                 Plate = plate,
-                                Position = platePosition++,
-                                OrderId = sortedOrders[currentJobIndex++].OrderId,
+                                Position = 1 + candidates.IndexOf(order),
+                                OrderId = order.OrderId,
                                 IsBlank = false
                             };
                             plateAssignments.Add(plateAssignment);
-                            sortedOrders.Remove(sortedOrders[currentJobIndex]);
+                            sortedOrders.Remove(sortedOrders[orderIndex]);
                         }
                     }
                     dbContext.Add(plate);
