9b47363633580647d40a35bb4216274ed8d11799
Ryan Stauffer <88114694+drdotnet22@users.noreply.github.com>
2025-10-30 16:32:02 -0400

    Printing platelist.

---
+++ wwwroot/site.js
@@ -0,0 +1,23 @@
+﻿window.printElement = (elementId) => {
+    const element = document.getElementById(elementId);
+    if (!element) return;
+
+    const printWindow = window.open('', '', 'width=800,height=600');
+    const doc = printWindow.document;
+
+    doc.write('<html><head><title>Print</title>');
+
+    // Copy all stylesheets and inline styles
+    const styles = Array.from(document.querySelectorAll('link[rel="stylesheet"], style'));
+    styles.forEach(style => {
+        doc.write(style.outerHTML);
+    });
+
+    doc.write('</head><body>');
+    doc.write(element.outerHTML);
+    doc.write('</body></html>');
+    doc.close();
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+};
